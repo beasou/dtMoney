777fccc51166100ac67623dc0d7ee8260529bd87
beasou <beatrizreginasouza1@gmail.com>
2022-05-30 15:08:01 -0300

    TransactionsTable

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,11 +1,14 @@
 import { Header } from "./components/Header"
 import { GlobalStyle } from "./styles/global"
+import { TransactionsTable } from "./components/TransactionsTable"
+import { Dashboard } from "./components/Dashboard/intex"
 
 export function App() {
   return (
     <>
       <Header/>
       <GlobalStyle />
+      <Dashboard/>
     </>
   )
 }
