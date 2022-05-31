790653b3f1666621096f137c583f49a168294d68
beasou <beatrizreginasouza1@gmail.com>
2022-05-31 12:13:11 -0300

    Configurando modal de criação

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,31 @@
+import Modal from 'react-modal'
+import { useState } from 'react'
 import { Header } from "./components/Header"
 import { GlobalStyle } from "./styles/global"
-import { TransactionsTable } from "./components/TransactionsTable"
 import { Dashboard } from "./components/Dashboard/intex"
 
+Modal.setAppElement('#root')//dentro de onde o modal vai se referenciar no caso na div root, mas o modal vai ficar no body
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <GlobalStyle />
       <Dashboard/>
+      <Modal isOpen={isNewTransactionModalOpen} 
+                       onRequestClose={handleCloseNewTransactionModal}>
+                    <h2>Cadastrar Transação</h2>
+      </Modal>
     </>
   )
 }
